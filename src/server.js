3a94090654977e1f69818ec5586520bf35057f30
tsuryanto <tsuryanto16@gmail.com>
2023-10-22 01:44:08 +0700

    fix typo production wording

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import { routes } from './routes.js';
 const init = async () => {
     const server = _server({
         port: 5000,
-        host: process.env.NODE_ENV !== 'producton' ? 'localhost' : '0.0.0.0',
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         routes: {
             cors: {
                 origin: ['*'],
